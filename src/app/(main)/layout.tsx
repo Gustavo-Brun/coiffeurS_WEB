55a3d91aaf91b65292ba83a301ece928eacafb8d
gustavo brun <gustavobdossantos.gb@gmail.com>
2025-07-22 19:20:40 -0300

    fix: remove dashboard module

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,12 +1,7 @@
 import { SideBar } from '@/app/(main)/@components/sidebar';
-import { ChartAreaInteractive } from '@/app/(main)/dashboard/components/ChartAreaInteractive';
-import { DataTable } from '@/app/(main)/dashboard/components/DataTable';
-import { SectionCards } from '@/app/(main)/dashboard/components/SectionCards';
 import { Header } from '@/app/(main)/@components/sidebar/Header';
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
 import React from 'react';
-import dynamic from 'next/dynamic';
-import Alert from '@/components/ui/alert';
 
 export default function DailyVetMainLayout({
   children
